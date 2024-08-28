6cda438ef35fec3fb614c7d644ddf9f6dbc6c017
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2024-08-28 15:59:47 +0300

    change baseUrl for api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseUrl = "https://builder-service.onrender.com/api";
-const baseUrl = "http://localhost:5000/api";
+const baseUrl = "https://builder-service.onrender.com/api";
+// const baseUrl = "http://localhost:5000/api";
 
 axios.interceptors.request.use(
   (config) => {
